7a28a3f5d85dec106ebe3f68fd6132ed1a736782
Yash Walankar <yashw5995@gmail.com>
2020-07-16 01:50:46 -0400

    end of lesson

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.postgress_username,
+    "password": process.env.postgress_password,
+    "database": process.env.postgress_database,
+    "host": process.env.postgress_host,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": process.env.aws_region,
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": process.env.aws_media_bucket
   },
   "prod": {
     "username": "",
